0630dbedeffd75cc7ac94d07dc1055100a5df653
Alexander Schenkel <aschenkel@kadenpartner.ch>
2023-12-01 17:11:11 +0100

    Preps for day 2, and some refactoring for day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,17 +1,23 @@
 use adventofcode2023::read_lines;
+use regex::Regex;
 
 use super::Day;
-
-const MAX_NR: u32 = 1000;
 
 #[derive(Debug)]
 pub struct Day01 {
     input: Vec<String>,
+    number_pattern: Regex,
 }
 
 impl Day01 {
     pub fn new() -> Day01 {
-        Day01 { input: Vec::new() }
+        Day01 {
+            input: Vec::new(),
+            number_pattern: regex::Regex::new(
+                r"(1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine)",
+            )
+            .unwrap(),
+        }
     }
 }
 
@@ -45,12 +51,12 @@ impl Day for Day01 {
     fn solve2(&self) -> String {
         let mut sum: u64 = 0;
         for line in self.input.iter() {
-            let all = findNrs(line);
+            let all = find_nrs(&self.number_pattern, line);
             if all.len() > 0 {
-                let firstStr = all[0].as_str();
-                let lastStr = all[all.len() - 1].as_str();
-                let first = strToNr(firstStr);
-                let last = strToNr(lastStr);
+                let first_str = all[0].as_str();
+                let last_str = all[all.len() - 1].as_str();
+                let first = str_to_nr(first_str);
+                let last = str_to_nr(last_str);
                 // if all.len() == 1 {
                 //     println!("single entry: {0}: {1}", all[0], line);
                 //     println!("{0} {1}", first, last);
@@ -62,27 +68,26 @@ impl Day for Day01 {
     }
 }
 
-fn findNrs(line: &str) -> Vec<String> {
+fn find_nrs(re: &Regex, line: &str) -> Vec<String> {
     // attention: we cannot just simply find all regex sub-groups, as there are some
-    // mean entries like "eighthree" or "sevenine"..... So I loop over all substring, removing
-    // one character at at time from the beginning.
-    let re = regex::Regex::new(r"(1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine)")
-        .unwrap();
+    // mean entries like "eighthree" or "sevenine"..... So I loop over all smaller substrings, removing
+    // one character at at time from the beginning, and test the remaining.
     let mut res = Vec::new();
-    let lower = line.to_lowercase();
+
     for m in 0..(line.len()) {
-        let sub: String = lower.chars().skip(m).collect();
-        let m = re.find(sub.as_str());
+        let sub = &line[m..];
+        let m = re.find(sub);
         if m.is_some() {
             let s = m.unwrap().as_str();
             res.push(String::from(s));
         }
     }
 
+    // contains now a list of all numbers found, including overlapping
     res
 }
 
-fn strToNr(line: &str) -> u64 {
+fn str_to_nr(line: &str) -> u64 {
     match line {
         "one" => 1,
         "1" => 1,
--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,33 +1,38 @@
-use super::Day;
+use adventofcode2023::read_lines;
 
-const MAX_NR: u32 = 1000;
+use super::Day;
 
 #[derive(Debug)]
 pub struct Day02 {
+    input: Vec<String>,
 }
 
 impl Day02 {
     pub fn new() -> Day02 {
-        Day02 { }
+        Day02 { input: Vec::new() }
     }
 }
 
 impl Day for Day02 {
     fn day_nr(&self) -> String {
-        String::from("01")
+        String::from("02")
     }
     fn title(&self) -> String {
-        String::from("Problem 1")
+        String::from("Day 2: ")
     }
 
     fn prepare(&mut self) {
-        // Nothing to do
+        // let input = read_lines("data/day02.txt");
+        let input = read_lines("data/day02-test.txt");
+        self.input = input;
     }
 
     fn solve1(&self) -> String {
-        String::from(format!("{0}", ""))
+        let mut sum: u32 = 0;
+        String::from(format!("{0}", sum))
     }
     fn solve2(&self) -> String {
-        String::from(format!("{0}", ""))
+        let mut sum: u64 = 0;
+        String::from(format!("{0}", sum))
     }
 }
