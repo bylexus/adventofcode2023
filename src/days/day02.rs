623f7dcc347e256eaa146b9c0c708ff189bdade0
Alexander Schenkel <aschenkel@kadenpartner.ch>
2023-12-02 08:17:12 +0100

    Day 02 - refactored
    Parsing is now separated to the preparation stage, so that solution 1 + 2 only have to do the real work.

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -6,11 +6,61 @@ use super::Day;
 #[derive(Debug)]
 pub struct Day02 {
     input: Vec<String>,
+    games: Vec<Game>,
+}
+
+#[derive(Debug)]
+struct Game {
+    id: u32,
+    groups: Vec<(u32, String)>,
 }
 
 impl Day02 {
     pub fn new() -> Day02 {
-        Day02 { input: Vec::new() }
+        Day02 {
+            input: Vec::new(),
+            games: Vec::new(),
+        }
+    }
+
+    fn parse_input(&mut self) {
+        let game_id_match = Regex::new(r"Game (\d+):").unwrap();
+        let group_match = Regex::new(r"(\d+) (\w+)").unwrap();
+        for line in self.input.iter() {
+            let mut game = Game {
+                id: 0,
+                groups: Vec::new(),
+            };
+
+            // extract game id:
+            let game_id_opt = game_id_match.captures(line);
+            if !game_id_opt.is_some() {
+                continue;
+            }
+            let game_id_str = game_id_opt.unwrap().get(1).unwrap().as_str();
+            let game_id = game_id_str.parse::<u32>().unwrap();
+            game.id = game_id;
+
+            // rest of string:
+            let rest = &line[(line.find(':').unwrap() + 1)..];
+
+            // split groups:
+            let groups: Vec<String> = rest.split(';').map(|m| m.to_string()).collect();
+
+            // parse each group:
+            for group in groups {
+                group_match
+                    .captures_iter(group.as_str())
+                    .map(|m| {
+                        let (_, [a, b]) = m.extract();
+                        (a.parse::<u32>().unwrap(), b)
+                    })
+                    .for_each(|(nr, color)| {
+                        game.groups.push((nr, color.to_string()));
+                    });
+            }
+            self.games.push(game);
+        }
     }
 }
 
@@ -26,103 +76,51 @@ impl Day for Day02 {
         let input = read_lines("data/day02.txt");
         // let input = read_lines("data/day02-test.txt");
         self.input = input;
+        self.parse_input();
     }
 
     fn solve1(&self) -> String {
-        let game_id_match = Regex::new(r"Game (\d+):").unwrap();
-        let group_match = Regex::new(r"(\d+) (\w+)").unwrap();
         let mut sum = 0;
-        for line in self.input.iter() {
-            // extract game id:
-            let game_id_opt = game_id_match.captures(line);
-            if !game_id_opt.is_some() {
-                continue;
-            }
-            let game_id_str = game_id_opt.unwrap().get(1).unwrap().as_str();
-            let game_id = game_id_str.parse::<u32>().unwrap();
-
-            // rest of string:
-            let rest = &line[(line.find(':').unwrap() + 1)..];
-
-            // split groups:
-            let groups: Vec<String> = rest.split(';').map(|m| m.to_string()).collect();
-
-            // parse each group:
+        for game in self.games.iter() {
+            // Check if game is valid
             let mut valid = true;
-            for group in groups {
-                let parts: Vec<(u32, &str)> = group_match
-                    .captures_iter(group.as_str())
-                    .map(|m| {
-                        let (full, [a, b]) = m.extract();
-                        (a.parse::<u32>().unwrap(), b)
-                    })
-                    .collect();
-
-                // Check if game is valid
-                for part in parts {
-                    if part.1 == "red" && part.0 > 12 {
-                        valid = false;
-                        break;
-                    }
-                    if part.1 == "green" && part.0 > 13 {
-                        valid = false;
-                        break;
-                    }
-                    if part.1 == "blue" && part.0 > 14 {
-                        valid = false;
-                        break;
-                    }
+            for part in game.groups.iter() {
+                if part.1 == "red" && part.0 > 12 {
+                    valid = false;
+                    break;
+                }
+                if part.1 == "green" && part.0 > 13 {
+                    valid = false;
+                    break;
+                }
+                if part.1 == "blue" && part.0 > 14 {
+                    valid = false;
+                    break;
                 }
             }
             if valid {
-                sum += game_id;
+                sum += game.id;
             }
         }
         String::from(format!("{0}", sum))
     }
     fn solve2(&self) -> String {
-        let game_id_match = Regex::new(r"Game (\d+):").unwrap();
-        let group_match = Regex::new(r"(\d+) (\w+)").unwrap();
         let mut sum = 0;
-        for line in self.input.iter() {
-            // extract game id:
-            let game_id_opt = game_id_match.captures(line);
-            if !game_id_opt.is_some() {
-                continue;
-            }
-            let game_id_str = game_id_opt.unwrap().get(1).unwrap().as_str();
-            let game_id = game_id_str.parse::<u32>().unwrap();
-
-            // rest of string:
-            let rest = &line[(line.find(':').unwrap() + 1)..];
-
-            // split groups:
-            let groups: Vec<String> = rest.split(';').map(|m| m.to_string()).collect();
-
+        for game in self.games.iter() {
             // parse each group:
             let mut max_red = 0;
             let mut max_green = 0;
             let mut max_blue = 0;
-            for group in groups {
-                let parts: Vec<(u32, &str)> = group_match
-                    .captures_iter(group.as_str())
-                    .map(|m| {
-                        let (full, [a, b]) = m.extract();
-                        (a.parse::<u32>().unwrap(), b)
-                    })
-                    .collect();
-
+            for group in game.groups.iter() {
                 // Check if game is valid
-                for part in parts {
-                    if part.1 == "red" && part.0 > max_red {
-                        max_red = part.0;
-                    }
-                    if part.1 == "green" && part.0 > max_green {
-                        max_green = part.0;
-                    }
-                    if part.1 == "blue" && part.0 > max_blue {
-                        max_blue = part.0;
-                    }
+                if group.1 == "red" && group.0 > max_red {
+                    max_red = group.0;
+                }
+                if group.1 == "green" && group.0 > max_green {
+                    max_green = group.0;
+                }
+                if group.1 == "blue" && group.0 > max_blue {
+                    max_blue = group.0;
                 }
             }
             let power = max_red * max_green * max_blue;
