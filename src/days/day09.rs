bef7fee540ba268821288cdafda86191eb841250
Alexander Schenkel <aschenkel@kadenpartner.ch>
2023-12-09 09:47:55 +0100

    Day 09 - Solution 1 + 2, with a neat recursive function

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -1,8 +1,5 @@
-use std::{cell::RefCell, rc::Rc};
-
 use super::Day;
 use adventofcode2023::read_lines;
-use regex::Regex;
 
 #[derive(Debug)]
 pub struct Day09 {
@@ -21,7 +18,7 @@ impl Day09 {
     fn parse_input(&mut self) {
         for line in self.input.iter() {
             let mut entries = Vec::new();
-            for part in line.split_ascii_whitespace(){
+            for part in line.split_ascii_whitespace() {
                 let nr = part.parse::<i64>().unwrap();
                 entries.push(nr);
             }
@@ -49,40 +46,30 @@ impl Day for Day09 {
 
     fn solve1(&mut self) -> String {
         let mut solution: i64 = 0;
-        println!("Data: {:?}", self.data);
         for data_line in &self.data {
-            let mut only_zeros = false;
-            let mut diff_lines = Vec::new();
-            diff_lines.push((*data_line).clone());
-            // calc diff lines:
-            while !only_zeros {
-                let res = calc_diff_line(diff_lines.last().unwrap());
-                only_zeros = res.1;
-                // if res.0.len() == 0 {
-                //     diff_lines.push(Vec::from([0]));
-                // } else {
-                    diff_lines.push(res.0);
-                // }
-            }
-            // calc resulting last numbers:
-            println!("\n\nDiff lines: {:?}", diff_lines);
-            for i in (0..(diff_lines.len() - 1)).rev() {
-                let act_line = diff_lines[i].clone();
-                let next_line = diff_lines[i + 1].clone();
-                let last_el_act = act_line.last().unwrap();
-                let last_el_next = next_line.last().unwrap();
-                diff_lines[i].push(last_el_act + last_el_next);
-                println!("   Diff line: {:?}", diff_lines[i]);
-            }
-            solution += diff_lines[0].last().unwrap();
-            println!("Diff lines: {:?}", diff_lines);
+            solution += calc_next_nr(data_line);
         }
         String::from(format!("{0}", solution))
     }
 
     fn solve2(&mut self) -> String {
-        let mut solution: u64 = 0;
+        let mut solution: i64 = 0;
+        // same as 1, but just with reversed input:
+        for data_line in &self.data {
+            let rev: Vec<i64> = data_line.iter().rev().cloned().collect();
+            solution += calc_next_nr(&rev);
+        }
         String::from(format!("{0}", solution))
+    }
+}
+
+fn calc_next_nr(line: &Vec<i64>) -> i64 {
+    let (diff_line, only_zeros) = calc_diff_line(line);
+    let last = *line.last().unwrap();
+    if only_zeros {
+        return last;
+    } else {
+        return last + calc_next_nr(&diff_line);
     }
 }
 
