e34d4ac408c843d6d5f2c11a20c5cc3aca7a97fa
Alexander Schenkel <aschenkel@kadenpartner.ch>
2023-12-16 10:20:32 +0100

    Day 16 - Solutions 1 + 2
    Straight-forward walking through a 'maze' of sorts. I decided to use a breadth-first algorithm using a queue to add up the next moves.
    
    The 2nd solution could of course be optimized a bit, now it runs for about 3 seconds. I think I will try a Threaded version later in the day.

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -1,21 +1,182 @@
+use std::collections::{HashMap, VecDeque};
 
-use alex_lib::read_lines;
+use alex_lib::{
+    read_lines,
+    types::{Coord2d, Direction},
+};
 
 use super::Day;
+
+/// For each field cell, we need to keep track
+/// if the field was visited, and from which directions:
+/// a field does not need to be visited again if it is entered
+/// twice from the same direction (but can be visited from a non-visited direction)
+#[derive(Debug)]
+struct VisitedInfo {
+    visited_count: u64,
+    visited_dirs: Vec<Direction>,
+}
 
 #[derive(Debug)]
 pub struct Day16 {
     input: Vec<String>,
+    field: Vec<Vec<char>>,
 }
 
 impl Day16 {
     pub fn new() -> Day16 {
         Day16 {
             input: Vec::new(),
+            field: Vec::new(),
         }
     }
 
     fn parse_input(&mut self) {
+        for line in self.input.iter() {
+            let mut l = Vec::new();
+            for c in line.chars() {
+                l.push(c);
+            }
+            self.field.push(l);
+        }
+    }
+
+    /// executes a single move:
+    /// the coord is the new coordinate we are to step in,
+    /// the direction is the actual heading direction.
+    /// We just fill the queue with the next step(s), and return.
+    fn execute_move(
+        &mut self,
+        coord: Coord2d,
+        direction: Direction,
+        move_q: &mut VecDeque<(Coord2d, Direction)>,
+        visited: &mut HashMap<Coord2d, VisitedInfo>,
+    ) {
+        // we're out of the field, so we do nothing:
+        if coord.x < 0
+            || coord.y < 0
+            || coord.y >= self.field.len() as i64
+            || coord.x >= self.field[0].len() as i64
+        {
+            return;
+        }
+
+        // if we were here from the same direction already, stop here:
+        if let Some(v) = visited.get(&coord) {
+            if v.visited_dirs.contains(&direction) {
+                return;
+            }
+        }
+        // Mark pos as visited:
+        match visited.get_mut(&coord) {
+            Some(v) => {
+                v.visited_count += 1;
+                v.visited_dirs.push(direction);
+            }
+            None => {
+                visited.insert(
+                    coord,
+                    VisitedInfo {
+                        visited_count: 1,
+                        visited_dirs: vec![direction],
+                    },
+                );
+            }
+        }
+        // execute the move:
+        let floor = self.field[coord.y as usize][coord.x as usize];
+        match floor {
+            // . --> move forward
+            '.' => match direction {
+                Direction::UP => {
+                    move_q.push_back((coord.up(), direction));
+                }
+                Direction::RIGHT => {
+                    move_q.push_back((coord.right(), direction));
+                }
+                Direction::DOWN => {
+                    move_q.push_back((coord.down(), direction));
+                }
+                Direction::LEFT => {
+                    move_q.push_back((coord.left(), direction));
+                }
+            },
+            // - --> move forward if headed horizontal, spawn 2 dirs if headed vertical
+            '-' => {
+                if direction == Direction::UP || direction == Direction::DOWN {
+                    move_q.push_back((coord.right(), Direction::RIGHT));
+                    move_q.push_back((coord.left(), Direction::LEFT));
+                } else if direction == Direction::RIGHT {
+                    move_q.push_back((coord.right(), direction));
+                } else if direction == Direction::LEFT {
+                    move_q.push_back((coord.left(), direction));
+                } else {
+                    panic!("Unknown direction");
+                }
+            }
+            '|' => {
+                if direction == Direction::LEFT || direction == Direction::RIGHT {
+                    move_q.push_back((coord.up(), Direction::UP));
+                    move_q.push_back((coord.down(), Direction::DOWN));
+                } else if direction == Direction::UP {
+                    move_q.push_back((coord.up(), direction));
+                } else if direction == Direction::DOWN {
+                    move_q.push_back((coord.down(), direction));
+                } else {
+                    panic!("Unknown direction");
+                }
+            }
+            '/' => match direction {
+                Direction::UP => {
+                    move_q.push_back((coord.right(), Direction::RIGHT));
+                }
+                Direction::RIGHT => {
+                    move_q.push_back((coord.up(), Direction::UP));
+                }
+                Direction::DOWN => {
+                    move_q.push_back((coord.left(), Direction::LEFT));
+                }
+                Direction::LEFT => {
+                    move_q.push_back((coord.down(), Direction::DOWN));
+                }
+            },
+            '\\' => match direction {
+                Direction::UP => {
+                    move_q.push_back((coord.left(), Direction::LEFT));
+                }
+                Direction::RIGHT => {
+                    move_q.push_back((coord.down(), Direction::DOWN));
+                }
+                Direction::DOWN => {
+                    move_q.push_back((coord.right(), Direction::RIGHT));
+                }
+                Direction::LEFT => {
+                    move_q.push_back((coord.up(), Direction::UP));
+                }
+            },
+            _ => panic!("Unknown floor: {}", floor),
+        }
+    }
+
+    /// Starts the beam from the given start position and direction,
+    /// and calculates the energy of the beam.
+    ///
+    /// Instead of a recursive algorithm, I use a queue here: I insert the start position into the queue,
+    /// and calc the next steps until the queue is empty.
+    /// Each move calc might add more steps to the queue (0, 1 or 2 more), depending
+    /// on the mirror it steps on on its way.
+    fn calc_energy(&mut self, start_pos: Coord2d, start_dir: Direction) -> u64 {
+        let mut move_q = VecDeque::new();
+        // self.visited = HashMap::new();
+        let mut visited = HashMap::new();
+
+        move_q.push_back((start_pos, start_dir));
+        while move_q.len() > 0 {
+            let (move_to, direction) = move_q.pop_front().unwrap();
+            self.execute_move(move_to, direction, &mut move_q, &mut visited);
+        }
+
+        visited.len() as u64
     }
 }
 
@@ -24,23 +185,73 @@ impl Day for Day16 {
         String::from("16")
     }
     fn title(&self) -> String {
-        String::from("xxxxxx")
+        String::from("The Floor Will Be Lava")
     }
 
     fn prepare(&mut self) {
-        // let input = read_lines("data/day16.txt");
-        let input = read_lines("data/day16-test.txt");
-        self.input = input;
+        let input = read_lines("data/day16.txt");
+        // let input = read_lines("data/day16-test.txt");
+        self.input = input
+            .iter()
+            .filter(|f| !f.is_empty())
+            .map(|f| f.to_string())
+            .collect();
         self.parse_input();
     }
 
     fn solve1(&mut self) -> String {
-        let mut solution: u64 = 0;
+        let solution: u64;
+
+        let start_pos = Coord2d { x: 0, y: 0 };
+        let start_dir = Direction::RIGHT;
+        solution = self.calc_energy(start_pos, start_dir);
+
         String::from(format!("{0}", solution))
     }
 
     fn solve2(&mut self) -> String {
-        let mut solution: u64 = 0;
+        let solution: u64;
+        let mut max_energy: u64 = 0;
+        let mut start_positions: Vec<(Coord2d, Direction)> = Vec::new();
+
+        // start from top row:
+        for x in 0..self.field[0].len() as i64 {
+            start_positions.push((Coord2d { x, y: 0 }, Direction::DOWN));
+        }
+        // start from bottom row:
+        for x in 0..self.field[0].len() as i64 {
+            start_positions.push((
+                Coord2d {
+                    x,
+                    y: self.field.len() as i64 - 1,
+                },
+                Direction::UP,
+            ));
+        }
+        // start from left row:
+        for y in 0..self.field.len() as i64 {
+            start_positions.push((Coord2d { x: 0, y }, Direction::RIGHT));
+        }
+        // start from right row:
+        for y in 0..self.field.len() as i64 {
+            start_positions.push((
+                Coord2d {
+                    x: self.field[0].len() as i64 - 1,
+                    y,
+                },
+                Direction::LEFT,
+            ));
+        }
+
+        // now, for each start position, calculate the energy
+        for (start_pos, start_dir) in start_positions {
+            let res = self.calc_energy(start_pos, start_dir);
+            if res > max_energy {
+                max_energy = res;
+            }
+        }
+        solution = max_energy;
+
         String::from(format!("{0}", solution))
     }
 }
