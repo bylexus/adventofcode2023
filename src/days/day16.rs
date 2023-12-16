2d025e7b7713f9e1410a837a4d05613d71a9f490
Alexander Schenkel <aschenkel@kadenpartner.ch>
2023-12-16 15:42:26 +0100

    Day 16 - Threaded solution - runs 6 times faster
    I implemented a ThreadPool solution, and run the calculations on 10 parallel threads. Instead of 3s, it takes only 0.45s now.

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -1,7 +1,12 @@
-use std::collections::{HashMap, VecDeque};
+use log::{debug, error, log_enabled, info, Level};
+use std::{
+    collections::{HashMap, VecDeque},
+    sync::{Arc, Mutex}, thread::available_parallelism,
+};
 
 use alex_lib::{
     read_lines,
+    threads::ThreadPool,
     types::{Coord2d, Direction},
 };
 
@@ -40,144 +45,6 @@ impl Day16 {
             self.field.push(l);
         }
     }
-
-    /// executes a single move:
-    /// the coord is the new coordinate we are to step in,
-    /// the direction is the actual heading direction.
-    /// We just fill the queue with the next step(s), and return.
-    fn execute_move(
-        &mut self,
-        coord: Coord2d,
-        direction: Direction,
-        move_q: &mut VecDeque<(Coord2d, Direction)>,
-        visited: &mut HashMap<Coord2d, VisitedInfo>,
-    ) {
-        // we're out of the field, so we do nothing:
-        if coord.x < 0
-            || coord.y < 0
-            || coord.y >= self.field.len() as i64
-            || coord.x >= self.field[0].len() as i64
-        {
-            return;
-        }
-
-        // if we were here from the same direction already, stop here:
-        if let Some(v) = visited.get(&coord) {
-            if v.visited_dirs.contains(&direction) {
-                return;
-            }
-        }
-        // Mark pos as visited:
-        match visited.get_mut(&coord) {
-            Some(v) => {
-                v.visited_count += 1;
-                v.visited_dirs.push(direction);
-            }
-            None => {
-                visited.insert(
-                    coord,
-                    VisitedInfo {
-                        visited_count: 1,
-                        visited_dirs: vec![direction],
-                    },
-                );
-            }
-        }
-        // execute the move:
-        let floor = self.field[coord.y as usize][coord.x as usize];
-        match floor {
-            // . --> move forward
-            '.' => match direction {
-                Direction::UP => {
-                    move_q.push_back((coord.up(), direction));
-                }
-                Direction::RIGHT => {
-                    move_q.push_back((coord.right(), direction));
-                }
-                Direction::DOWN => {
-                    move_q.push_back((coord.down(), direction));
-                }
-                Direction::LEFT => {
-                    move_q.push_back((coord.left(), direction));
-                }
-            },
-            // - --> move forward if headed horizontal, spawn 2 dirs if headed vertical
-            '-' => {
-                if direction == Direction::UP || direction == Direction::DOWN {
-                    move_q.push_back((coord.right(), Direction::RIGHT));
-                    move_q.push_back((coord.left(), Direction::LEFT));
-                } else if direction == Direction::RIGHT {
-                    move_q.push_back((coord.right(), direction));
-                } else if direction == Direction::LEFT {
-                    move_q.push_back((coord.left(), direction));
-                } else {
-                    panic!("Unknown direction");
-                }
-            }
-            '|' => {
-                if direction == Direction::LEFT || direction == Direction::RIGHT {
-                    move_q.push_back((coord.up(), Direction::UP));
-                    move_q.push_back((coord.down(), Direction::DOWN));
-                } else if direction == Direction::UP {
-                    move_q.push_back((coord.up(), direction));
-                } else if direction == Direction::DOWN {
-                    move_q.push_back((coord.down(), direction));
-                } else {
-                    panic!("Unknown direction");
-                }
-            }
-            '/' => match direction {
-                Direction::UP => {
-                    move_q.push_back((coord.right(), Direction::RIGHT));
-                }
-                Direction::RIGHT => {
-                    move_q.push_back((coord.up(), Direction::UP));
-                }
-                Direction::DOWN => {
-                    move_q.push_back((coord.left(), Direction::LEFT));
-                }
-                Direction::LEFT => {
-                    move_q.push_back((coord.down(), Direction::DOWN));
-                }
-            },
-            '\\' => match direction {
-                Direction::UP => {
-                    move_q.push_back((coord.left(), Direction::LEFT));
-                }
-                Direction::RIGHT => {
-                    move_q.push_back((coord.down(), Direction::DOWN));
-                }
-                Direction::DOWN => {
-                    move_q.push_back((coord.right(), Direction::RIGHT));
-                }
-                Direction::LEFT => {
-                    move_q.push_back((coord.up(), Direction::UP));
-                }
-            },
-            _ => panic!("Unknown floor: {}", floor),
-        }
-    }
-
-    /// Starts the beam from the given start position and direction,
-    /// and calculates the energy of the beam.
-    ///
-    /// Instead of a recursive algorithm, I use a queue here: I insert the start position into the queue,
-    /// and calc the next steps until the queue is empty.
-    /// Each move calc might add more steps to the queue (0, 1 or 2 more), depending
-    /// on the mirror it steps on on its way.
-    fn calc_energy(&mut self, start_pos: Coord2d, start_dir: Direction) -> u64 {
-        let mut move_q = VecDeque::new();
-        // self.visited = HashMap::new();
-        let mut visited = HashMap::new();
-
-        move_q.push_back((start_pos, start_dir));
-        while move_q.len() > 0 {
-            let (move_to, direction) = move_q.pop_front().unwrap();
-            self.execute_move(move_to, direction, &mut move_q, &mut visited);
-        }
-
-        visited.len() as u64
-    }
 }
 
 impl Day for Day16 {
@@ -189,6 +56,7 @@ impl Day for Day16 {
     }
 
     fn prepare(&mut self) {
+        env_logger::init();
         let input = read_lines("data/day16.txt");
         // let input = read_lines("data/day16-test.txt");
         self.input = input
@@ -204,14 +72,13 @@ impl Day for Day16 {
 
         let start_pos = Coord2d { x: 0, y: 0 };
         let start_dir = Direction::RIGHT;
-        solution = self.calc_energy(start_pos, start_dir);
+        solution = calc_energy(start_pos, start_dir, &self.field);
 
         String::from(format!("{0}", solution))
     }
 
     fn solve2(&mut self) -> String {
         let solution: u64;
-        let mut max_energy: u64 = 0;
         let mut start_positions: Vec<(Coord2d, Direction)> = Vec::new();
 
         // start from top row:
@@ -243,15 +110,167 @@ impl Day for Day16 {
             ));
         }
 
+        let parallels = match available_parallelism() {
+            Ok(n) => n.get(),
+            Err(_) => 1
+        };
+        let mut tpool = ThreadPool::new(parallels);
+        let results = Arc::new(Mutex::new(Vec::new()));
+
+
         // now, for each start position, calculate the energy
         for (start_pos, start_dir) in start_positions {
-            let res = self.calc_energy(start_pos, start_dir);
-            if res > max_energy {
-                max_energy = res;
-            }
+            let res = results.clone();
+            let field = self.field.clone();
+            tpool.enqueue(move |_| {
+                let ret = calc_energy(start_pos, start_dir, &field);
+                let mut res_arr = res.lock().unwrap();
+                res_arr.push(ret);
+            });
         }
-        solution = max_energy;
+        tpool.graceful_shutdown();
+
+        // fetch the max solution out of our shared solution pool:
+        solution = *results.lock().unwrap().iter().max().unwrap();
 
         String::from(format!("{0}", solution))
     }
+}
+
+/// executes a single move:
+/// the coord is the new coordinate we are to step in,
+/// the direction is the actual heading direction.
+/// We just fill the queue with the next step(s), and return.
+fn execute_move(
+    coord: Coord2d,
+    direction: Direction,
+    move_q: &mut VecDeque<(Coord2d, Direction)>,
+    visited: &mut HashMap<Coord2d, VisitedInfo>,
+    field: &Vec<Vec<char>>,
+) {
+    // we're out of the field, so we do nothing:
+    if coord.x < 0
+        || coord.y < 0
+        || coord.y >= field.len() as i64
+        || coord.x >= field[0].len() as i64
+    {
+        return;
+    }
+
+    // if we were here from the same direction already, stop here:
+    if let Some(v) = visited.get(&coord) {
+        if v.visited_dirs.contains(&direction) {
+            return;
+        }
+    }
+    // Mark pos as visited:
+    match visited.get_mut(&coord) {
+        Some(v) => {
+            v.visited_count += 1;
+            v.visited_dirs.push(direction);
+        }
+        None => {
+            visited.insert(
+                coord,
+                VisitedInfo {
+                    visited_count: 1,
+                    visited_dirs: vec![direction],
+                },
+            );
+        }
+    }
+    // execute the move:
+    let floor = field[coord.y as usize][coord.x as usize];
+    match floor {
+        // . --> move forward
+        '.' => match direction {
+            Direction::UP => {
+                move_q.push_back((coord.up(), direction));
+            }
+            Direction::RIGHT => {
+                move_q.push_back((coord.right(), direction));
+            }
+            Direction::DOWN => {
+                move_q.push_back((coord.down(), direction));
+            }
+            Direction::LEFT => {
+                move_q.push_back((coord.left(), direction));
+            }
+        },
+        // - --> move forward if headed horizontal, spawn 2 dirs if headed vertical
+        '-' => {
+            if direction == Direction::UP || direction == Direction::DOWN {
+                move_q.push_back((coord.right(), Direction::RIGHT));
+                move_q.push_back((coord.left(), Direction::LEFT));
+            } else if direction == Direction::RIGHT {
+                move_q.push_back((coord.right(), direction));
+            } else if direction == Direction::LEFT {
+                move_q.push_back((coord.left(), direction));
+            } else {
+                panic!("Unknown direction");
+            }
+        }
+        '|' => {
+            if direction == Direction::LEFT || direction == Direction::RIGHT {
+                move_q.push_back((coord.up(), Direction::UP));
+                move_q.push_back((coord.down(), Direction::DOWN));
+            } else if direction == Direction::UP {
+                move_q.push_back((coord.up(), direction));
+            } else if direction == Direction::DOWN {
+                move_q.push_back((coord.down(), direction));
+            } else {
+                panic!("Unknown direction");
+            }
+        }
+        '/' => match direction {
+            Direction::UP => {
+                move_q.push_back((coord.right(), Direction::RIGHT));
+            }
+            Direction::RIGHT => {
+                move_q.push_back((coord.up(), Direction::UP));
+            }
+            Direction::DOWN => {
+                move_q.push_back((coord.left(), Direction::LEFT));
+            }
+            Direction::LEFT => {
+                move_q.push_back((coord.down(), Direction::DOWN));
+            }
+        },
+        '\\' => match direction {
+            Direction::UP => {
+                move_q.push_back((coord.left(), Direction::LEFT));
+            }
+            Direction::RIGHT => {
+                move_q.push_back((coord.down(), Direction::DOWN));
+            }
+            Direction::DOWN => {
+                move_q.push_back((coord.right(), Direction::RIGHT));
+            }
+            Direction::LEFT => {
+                move_q.push_back((coord.up(), Direction::UP));
+            }
+        },
+        _ => panic!("Unknown floor: {}", floor),
+    }
+}
+
+/// Starts the beam from the given start position and direction,
+/// and calculates the energy of the beam.
+///
+/// Instead of a recursive algorithm, I use a queue here: I insert the start position into the queue,
+/// and calc the next steps until the queue is empty.
+/// Each move calc might add more steps to the queue (0, 1 or 2 more), depending
+/// on the mirror it steps on on its way.
+fn calc_energy(start_pos: Coord2d, start_dir: Direction, field: &Vec<Vec<char>>) -> u64 {
+    let mut move_q = VecDeque::new();
+    // self.visited = HashMap::new();
+    let mut visited = HashMap::new();
+
+    move_q.push_back((start_pos, start_dir));
+    while move_q.len() > 0 {
+        let (move_to, direction) = move_q.pop_front().unwrap();
+        execute_move(move_to, direction, &mut move_q, &mut visited, field);
+    }
+
+    visited.len() as u64
 }
