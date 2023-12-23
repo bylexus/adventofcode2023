549cc225c73cd56ce8135074e03b5ed3abfb6a05
Alexander Schenkel <aschenkel@kadenpartner.ch>
2023-12-23 14:53:44 +0100

    Day 23 - Part 1 - correct approach for part 1, but not for part 2
    Need some kind of memoization for part 2 - brute force dfs does not work

--- src/days/day23.rs
+++ src/days/day23.rs
@@ -1,10 +1,14 @@
 use core::panic;
-use std::{cell::RefCell, collections::HashSet, rc::Rc};
+use std::{
+    cell::RefCell,
+    collections::{HashMap, HashSet},
+    rc::Rc,
+};
 
 use super::Day;
 use alex_lib::{
     read_lines,
-    types::{Coord2d, Coord2dMap, Direction},
+    types::{Coord2d, Coord2dMap},
 };
 
 #[derive(Debug, Clone, Copy)]
@@ -18,7 +22,6 @@ enum FieldType {
 struct FieldInfo {
     chr: char,
     field_type: FieldType,
-    total_steps: u64,
     coord: Coord2d,
 }
 
@@ -28,7 +31,7 @@ pub struct Day23 {
     field: Coord2dMap<Rc<RefCell<FieldInfo>>>,
     start: Option<Coord2d>,
     end: Option<Coord2d>,
-    visited: HashSet<Coord2d>,
+    path_memo: RefCell<HashMap<Coord2d, Option<u64>>>,
 }
 
 impl Day23 {
@@ -38,7 +41,7 @@ impl Day23 {
             field: Coord2dMap::new(),
             start: None,
             end: None,
-            visited: HashSet::new(),
+            path_memo: RefCell::new(HashMap::new()),
         }
     }
 
@@ -52,7 +55,6 @@ impl Day23 {
                 // Detect start position:
                 if y == 0 && c == '.' {
                     self.start = Some(coord);
-                    self.visited.insert(coord);
                 }
                 // Detect end position:
                 if y == self.input.len() - 1 && c == '.' {
@@ -67,7 +69,6 @@ impl Day23 {
                             '#' => FieldType::Forest,
                             _ => FieldType::Slope(c),
                         },
-                        total_steps: 0,
                         coord,
                     })),
                 );
@@ -76,60 +77,107 @@ impl Day23 {
     }
 
     fn print_field(&self) {
-        let mut visited_counter = 0;
         for y in 0..self.input.len() {
             for x in 0..self.input[0].len() {
                 let coord = Coord2d {
                     x: x as i64,
                     y: y as i64,
                 };
-                let field = self.field.get(&coord).unwrap().borrow();
-                if self.visited.contains(&coord) {
-                    print!("O");
-                    visited_counter += 1;
-                } else {
-                    print!("{}", field.chr);
-                }
+                let field = self.field.get(&coord).unwrap().as_ref().borrow();
+                print!("{}", field.chr);
             }
             println!()
         }
-        println!("Visited: {}\n", visited_counter);
+        println!();
     }
 
-    fn find_unvisited_neighbour_nodes(&self, act_node: &FieldInfo) -> Vec<Rc<RefCell<FieldInfo>>> {
+    fn find_unvisited_neighbour_nodes(
+        &self,
+        act_node: &FieldInfo,
+        visited: &HashSet<Coord2d>,
+        treat_slopes_as_path: bool,
+    ) -> Vec<Rc<RefCell<FieldInfo>>> {
         let mut res: Vec<Rc<RefCell<FieldInfo>>> = Vec::new();
         let mut next_coords: Vec<Coord2d> = Vec::new();
 
-        match act_node.field_type {
-            FieldType::Path => {
-                next_coords.push(act_node.coord.up());
-                next_coords.push(act_node.coord.right());
-                next_coords.push(act_node.coord.down());
-                next_coords.push(act_node.coord.left());
-            }
-            FieldType::Slope(s) => match s {
-                '>' => next_coords.push(act_node.coord.right()),
-                '^' => next_coords.push(act_node.coord.up()),
-                '<' => next_coords.push(act_node.coord.left()),
-                'v' => next_coords.push(act_node.coord.down()),
-                _ => panic!("Unknown slope type: {}", s),
-            },
-            _ => {
-                panic!("Unsupported field type: {:?}", act_node.field_type)
+        if treat_slopes_as_path {
+            next_coords.push(act_node.coord.up());
+            next_coords.push(act_node.coord.right());
+            next_coords.push(act_node.coord.down());
+            next_coords.push(act_node.coord.left());
+        } else {
+            match act_node.field_type {
+                FieldType::Path => {
+                    next_coords.push(act_node.coord.up());
+                    next_coords.push(act_node.coord.right());
+                    next_coords.push(act_node.coord.down());
+                    next_coords.push(act_node.coord.left());
+                }
+                FieldType::Slope(s) => match s {
+                    '>' => next_coords.push(act_node.coord.right()),
+                    '^' => next_coords.push(act_node.coord.up()),
+                    '<' => next_coords.push(act_node.coord.left()),
+                    'v' => next_coords.push(act_node.coord.down()),
+                    _ => panic!("Unknown slope type: {}", s),
+                },
+                _ => {
+                    panic!("Unsupported field type: {:?}", act_node.field_type)
+                }
             }
         }
 
         for coord in next_coords {
             if let Some(node) = self.field.get(&coord) {
                 // add node to the next visitable list of nodes, if not yet visited:
-                let n = (*node).borrow();
-                if !self.visited.contains(&coord) && n.chr != '#' {
+                let n = (*node.as_ref()).borrow();
+                if !visited.contains(&coord) && n.chr != '#' {
                     res.push(Rc::clone(&node));
                 }
             }
         }
 
         res
+    }
+
+    fn find_longest_path(
+        &self,
+        field: &FieldInfo,
+        visited: &HashSet<Coord2d>,
+        treat_slopes_as_path: bool,
+    ) -> u64 {
+        if field.coord == self.end.unwrap() {
+            return visited.len() as u64;
+        }
+        // if let Some(memo) = self.path_memo.borrow().get(&field.coord) {
+        //     if let Some(memo) = *memo {
+        //         return memo;
+        //     }
+        // }
+
+        // let actual_memo_coords = {
+        //     let m = self.path_memo.borrow();
+        //     m.keys().cloned().collect::<Vec<Coord2d>>()
+        // };
+
+        let mut my_visited = visited.clone();
+        my_visited.insert(field.coord);
+        let next_nodes =
+            self.find_unvisited_neighbour_nodes(field, &my_visited, treat_slopes_as_path);
+        let mut max = 0;
+        for n in next_nodes {
+            max = std::cmp::max(
+                max,
+                self.find_longest_path(&(*n).borrow(), &my_visited, treat_slopes_as_path),
+            );
+            // let mut memo = self.path_memo.borrow_mut();
+            // for c in actual_memo_coords.iter() {
+            //     if let Some(m) = memo.get_mut(c) {
+            //         m.take();
+            //     }
+            // }
+        }
+        // self.path_memo.borrow_mut().insert(field.coord, Some(max));
+        return max;
     }
 }
 
@@ -142,8 +190,8 @@ impl Day for Day23 {
     }
 
     fn prepare(&mut self) {
-        // let input = read_lines("data/day23.txt");
-        let input = read_lines("data/day23-test.txt");
+        let input = read_lines("data/day23.txt");
+        // let input = read_lines("data/day23-test.txt");
         self.input = input
             .iter()
             .map(|x| x.trim().to_string())
@@ -153,59 +201,45 @@ impl Day for Day23 {
     }
 
     fn solve1(&mut self) -> String {
-        let mut solution: u64 = 0;
-        let mut visit_queue: Vec<Rc<RefCell<FieldInfo>>> = Vec::new();
+        // We cannot use Djikstra here, as we're looking for the LONGEST path.
+        // So we need a backtracking algorithm, which tracks ALL available paths....
+        // I use a recursive dfs approach:
+        // 1. start at the start position, mark it as visited
+        // 2. recursively calculate the path length of the next available nodes,
+        //    skipping the ones in my local visited list: the visited list is the local node's
+        //    list of previous nodes.
+        // This leads to many paths and copied visited lists, but who cares :-)
+
+        let solution: u64;
         let start_pos = self.start.unwrap();
-        let end_pos = self.end.unwrap();
+        let start_field_rc = self.field.get(&start_pos).unwrap();
+        let start_field = start_field_rc.as_ref().borrow();
+        let mut start_visited = HashSet::new();
+        start_visited.insert(start_pos);
 
         self.print_field();
         println!("Start: {:?}", self.start.unwrap());
         println!("End: {:?}", self.end.unwrap());
 
-        // we put the start node in the queue for the first iteration
-        let start_field = self.field.get(&start_pos).unwrap();
-        visit_queue.push(Rc::clone(&start_field));
-
-        let mut act_field_rc: Rc<RefCell<FieldInfo>>;
-        while visit_queue.len() > 0 {
-            {
-                // read last node from queue (queue must be sorted, as we pop from the end):
-                // node with most steps is at the end:
-                act_field_rc = visit_queue.pop().unwrap();
-                let mut act_field = act_field_rc.borrow_mut();
-                let act_pos = act_field.coord;
-
-                // Found the exit, end!
-                // if act_pos == end_pos {
-                //     break;
-                // }
-
-                // find unvisited nodes that can be visited:
-                let next_nodes = self.find_unvisited_neighbour_nodes(&act_field);
-
-                // add them to the queue, and sort the queue so that the nodes with the most steps is at the end:
-                for next_node in next_nodes {
-                    let mut n = next_node.borrow_mut();
-                    if n.total_steps < (act_field.total_steps + 1) {
-                        n.total_steps = act_field.total_steps + 1;
-                    }
-                    visit_queue.push(Rc::clone(&next_node));
-                }
-
-                // mark actual node as visited:
-                self.visited.insert(act_pos);
-            }
-            visit_queue.sort_by(|a, b| a.borrow().total_steps.cmp(&b.borrow().total_steps));
-        }
-
-        self.print_field();
-        println!("End field: {:?}", self.field.get(&end_pos).unwrap());
+        solution = self.find_longest_path(&start_field, &start_visited, false);
 
         String::from(format!("{0}", solution))
     }
 
     fn solve2(&mut self) -> String {
-        let mut solution: u64 = 0;
+        let solution: u64;
+        let start_pos = self.start.unwrap();
+        let start_field_rc = self.field.get(&start_pos).unwrap();
+        let start_field = start_field_rc.as_ref().borrow();
+        let mut start_visited = HashSet::new();
+        start_visited.insert(start_pos);
+
+        self.print_field();
+        println!("Start: {:?}", self.start.unwrap());
+        println!("End: {:?}", self.end.unwrap());
+
+        solution = self.find_longest_path(&start_field, &start_visited, true);
+
         String::from(format!("{0}", solution))
     }
 }
